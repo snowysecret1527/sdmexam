8a0fc7770628fc8b69b31b99a1779f7166e5cd02
snowysecret1527 <121279804+snowysecret1527@users.noreply.github.com>
2025-01-09 12:19:33 +0530

    Addition

---
+++ caccl.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class test{
+public static void main(String[] args)
+{
+Scanner sc=new Scanner(System.in);
+int a=sc.nextInt();
+int b=sc.nextInt();
+int sum=a+b;
+System.out.println("The addittion of two numbeeeeeeer is: "+sum);
+}
+}

aaed25fc297a1ec4ecc5bb4b9fbdb5af29f7608b
dac <dac@dac.myguest.virtualbox.org>
2025-01-09 12:25:03 +0530

    CommitFromLocal

--- caccl.java
+++ caccl.java
@@ -5,7 +5,7 @@ public static void main(String[] args)
 Scanner sc=new Scanner(System.in);
 int a=sc.nextInt();
 int b=sc.nextInt();
-int sum=a+b;
-System.out.println("The addittion of two numbeeeeeeer is: "+sum);
+int mul =a*b;
+System.out.println("The addittion of two number is: "+mul);
 }
 }
